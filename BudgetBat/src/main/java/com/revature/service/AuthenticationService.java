691dfbec0c30a1c143834890eec4b4d881fe480f
Adam Kager <atksource>
2019-08-02 07:12:06 -0400

    UserService and more session work

--- BudgetBat/src/main/java/com/revature/service/AuthenticationService.java
+++ BudgetBat/src/main/java/com/revature/service/AuthenticationService.java
@@ -1,5 +1,8 @@
 package com.revature.service;
 
+import java.util.ArrayList;
+
+import com.revature.beans.Credentials;
 import com.revature.beans.User;
 
 public class AuthenticationService {
@@ -15,20 +18,17 @@ public class AuthenticationService {
 		System.out.println("trying to getUsers from UserDaoImpl");
 		UserDaoImpl udi = new UserDaoImpl();
 		ArrayList<User> currentUsers = udi.getUsers();
-		System.out.println("checking if entry: " + creds.getUsername() + " is in database (not checking password right now)");
-		if(UserService.UsersIndex.contains(creds.getUsername()) && creds.getPassword().equals(
-				UserService.Users.get(creds.getUsername()).getPassword())) {
+		System.out.println("checking if entry: " + creds.getEmail() + " is in database (not checking password right now)");
+		if(UserService.UsersIndex.contains(creds.getEmail()) && creds.getPassword().equals(
+				UserService.Users.get(creds.getEmail()).getPassword())) {
 
-			System.out.println(creds.getUsername() + " is in the database and password matches!!!!!!");
-			user = UserService.Users.get(creds.getUsername());
+			System.out.println(creds.getEmail() + " is in the database and password matches!!!!!!");
+			user = UserService.Users.get(creds.getEmail());
 			
 		} else {
-			System.out.println(creds.getUsername() + " was not found!!!!!!");
+			System.out.println(creds.getEmail() + " was not found!!!!!!");
 		}
 		return user;
 	}
 
-}
-
-	
 }
