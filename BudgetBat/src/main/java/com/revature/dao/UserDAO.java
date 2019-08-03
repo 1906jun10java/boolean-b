6f6b3843ff5fdea2bb4c1b729c1af1b54604db55
Matthew Robertson <matt.robertson904@gmail.com>
2019-08-03 14:59:25 -0400

    sat push 2:59

--- BudgetBat/src/main/java/com/revature/dao/UserDAO.java
+++ BudgetBat/src/main/java/com/revature/dao/UserDAO.java
@@ -6,12 +6,12 @@ import com.revature.beans.User;
 
 public interface UserDAO {
 
-	public User getUserById(int id);
+	public User getUserByEmail(int email);
 	
 	public List<User> getAllUsers();
 	
 	public boolean addUser(User book);
-	
+
 	public boolean updateUser(User book);
 	
 	public boolean deleteUser(User book);
