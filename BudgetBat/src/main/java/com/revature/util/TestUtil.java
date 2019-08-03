776123158c9b877ab6daa52b0886d07729db5595
shyamuu1 <shyamuu.vasanjee@gmail.com>
2019-08-03 16:47:25 -0400

    webxml update

--- BudgetBat/src/main/java/com/revature/util/TestUtil.java
+++ BudgetBat/src/main/java/com/revature/util/TestUtil.java
@@ -5,11 +5,14 @@ import java.util.Properties;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
-
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+@Configuration
+@EnableTransactionManagement
 public class TestUtil {
 	
 	@Bean
